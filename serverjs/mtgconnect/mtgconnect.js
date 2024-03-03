8f833cfa37c96f93be2152ee0d41b712ea48df4a
dekkerglen <dekker.glen@gmail.com>
2024-03-03 14:45:59 -0500

    Update mtgconnect.js

--- serverjs/mtgconnect/mtgconnect.js
+++ serverjs/mtgconnect/mtgconnect.js
@@ -34,52 +34,58 @@ const theseCardsAreDistinct = (list) => {
 const newConnections = async (seed) => {
   seedrandom(seed, { global: true });
 
+  const pool = shuffle(nDistinctRandom(allOracles, 500));
+
+  const detailedCards = await Promise.all(
+    pool.map(async (card) => {
+      try {
+        const cobraresult = await fetch(`https://cubecobra.com/tool/cardjson/${card}`);
+        console.log(`https://cubecobra.com/tool/cardjson/${card}`);
+        return cobraresult.json();
+      } catch (e) {
+        return null;
+      }
+    }),
+  );
+
+  // sort by most played, filter out cards with no history
+  const filteredCards = detailedCards
+    .filter(
+      (details) =>
+        details && details.history && details.history.length > 0 && details.history[details.history.length - 1].total,
+    )
+    .sort((a, b) => {
+      // get the last history object
+      const aHistory = a.history[a.history.length - 1];
+      const bHistory = b.history[b.history.length - 1];
+
+      const aTotal = aHistory.total[0];
+      const bTotal = bHistory.total[0];
+
+      return bTotal - aTotal;
+    })
+    .map((details) => ({
+      name: details.card.name,
+      imageUrl: details.card.image_normal,
+      synergistic: details.synergistic.top.slice(0, 4).map((item) => ({
+        name: item.name,
+        imageUrl: item.image_normal,
+      })),
+    }));
+
   let result = [];
 
-  do {
-    // select 100 cards, we will pick the 4 most played cards that do not have overlapping synergies
-    const pool = shuffle(nDistinctRandom(allOracles, 200));
+  for (const item of filteredCards) {
+    const newResults = [...result, item];
 
-    const detailedCards = await Promise.all(
-      pool.map(async (card) => {
-        try {
-          const cobraresult = await fetch(`https://cubecobra.com/tool/cardjson/${card}`);
-          return cobraresult.json();
-        } catch (e) {
-          return null;
-        }
-      }),
-    );
+    if (theseCardsAreDistinct(newResults.map((details) => details.synergistic.map((d) => d.name)).flat())) {
+      result = newResults;
+    }
 
-    // sort by most played, filter out cards with no history
-    detailedCards
-      .filter(
-        (details) =>
-          details && details.history && details.history.length > 0 && details.history[details.history.length - 1].total,
-      )
-      .sort((a, b) => {
-        // get the last history object
-        const aHistory = a.history[a.history.length - 1];
-        const bHistory = b.history[b.history.length - 1];
-
-        const aTotal = aHistory.total[0];
-        const bTotal = bHistory.total[0];
-
-        return bTotal - aTotal;
-      });
-
-    // get the 4 most played cards
-    result = detailedCards.slice(0, 4).map((details) => {
-      return {
-        name: details.card.name,
-        imageUrl: details.card.image_normal,
-        synergistic: details.synergistic.top.slice(0, 4).map((item) => ({
-          name: item.name,
-          imageUrl: item.image_normal,
-        })),
-      };
-    });
-  } while (!theseCardsAreDistinct(result.map((card) => card.synergistic.map((item) => item.name)).flat()));
+    if (result.length >= 4) {
+      break;
+    }
+  }
 
   return result;
 };
